a575967b3efc04a68483e50c4091c60e9e0da643
Brian McCutchon <bmccutchon@udallas.edu>
2014-12-06 20:27:33 -0600

    Create test suite to hold all tests
    It's the one test to rule them all!

---
+++ Pool3D/src/com/brianmccutchon/pool3d/AllTests.java
@@ -0,0 +1,11 @@
+package com.brianmccutchon.pool3d;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PhysTest.class, PoolBallTest.class })
+public class AllTests {
+
+}
