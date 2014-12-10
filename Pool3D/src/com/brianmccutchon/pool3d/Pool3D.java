7deec7b6ade7e04acc578f03e0a22dadb0c55784
Brian McCutchon <bmccutchon@udallas.edu>
2014-12-10 14:11:43 -0600

    Improve camera movement
    Use a HashMap to store handlers (Runnables) for specific keys. Change
    the key mappings and add more motion commands.

--- Pool3D/src/com/brianmccutchon/pool3d/Pool3D.java
+++ Pool3D/src/com/brianmccutchon/pool3d/Pool3D.java
@@ -2,8 +2,10 @@ package com.brianmccutchon.pool3d;
 
 import java.awt.*;
 import java.awt.event.KeyAdapter;
+import static java.awt.event.KeyEvent.*;
 import java.awt.event.KeyEvent;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 
 import javax.swing.*;
@@ -15,40 +17,48 @@ public class Pool3D extends JPanel {
 
 	private static final long serialVersionUID = 2316556066963532682L;
 	Environment env;
-	static HashSet<Character> keysDown = new HashSet<>();
-	
+	static HashSet<Integer> keysDown = new HashSet<>();
+
+	HashMap<Integer, Runnable> handlers = new HashMap<>();
+
 	Timer t;
 	private long lastTime;
 
 	public Pool3D() {
 		env = new Environment();
-		//PoolBall ball = new PoolBall(0, 0, 0, null, null, 0, 5);
-		//env.addObject(ball);
 		Arrays.asList(Physics.balls).forEach(env::addObject);
 		Physics.balls[0].velocity.x = -0.4;
 		env.ambientLight = 0.5;
 		env.tempLightSource = new Point3D(500, -1000, 200);
+		
+		handlers.put(VK_RIGHT, env::rotateRight);
+		handlers.put(VK_LEFT,  env::rotateLeft);
+		handlers.put(VK_DOWN,  env::moveBackward);
+		handlers.put(VK_UP,    env::moveForward);
+		handlers.put(VK_S,     env::moveDown);
+		handlers.put(VK_W,     env::moveUp);
+		handlers.put(VK_D,     env::moveRight);
+		handlers.put(VK_A,     env::moveLeft);
+		handlers.put(VK_OPEN_BRACKET,  env::nearFarther);
+		handlers.put(VK_CLOSE_BRACKET, env::nearCloser);
 
 		t = new Timer(16, (e) -> {
 			long time = System.currentTimeMillis();
 			Physics.nextFrame(time - lastTime);
 
-			if(keysDown.contains('d'))
-				env.rotateRight();
-			if(keysDown.contains('a'))
-				env.rotateLeft();
-			if(keysDown.contains('s'))
-				env.moveBackward();
-			if(keysDown.contains('w'))
-				env.moveForward();
-			if(keysDown.contains('j'))
-				env.moveDown();
-			if(keysDown.contains('l'))
-				env.moveUp();
-			if(keysDown.contains('['))
-				env.nearFarther();
-			if(keysDown.contains(']'))
-				env.nearCloser();
+			for (int i : keysDown) {
+				if (handlers.containsKey(i)) {
+					handlers.get(i).run();
+				}
+			}
+
+			/* This works too
+			keysDown.stream()
+				.filter(handlers::containsKey)
+				.map(handlers::get)
+				.forEach(Runnable::run);
+			*/
+
 			repaint();
 		});
 
@@ -72,12 +82,12 @@ public class Pool3D extends JPanel {
 		frame.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent ke) {
-				keysDown.add(ke.getKeyChar());
+				keysDown.add(ke.getKeyCode());
 			}
 
 			@Override
 			public void keyReleased(KeyEvent ke) {
-				keysDown.remove(ke.getKeyChar());
+				keysDown.remove(ke.getKeyCode());
 			}
 		});
 	}
